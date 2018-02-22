ae03685ec8a6777c75d2d1e2abcfe548e2bd5aa0
Grzegorz 'Uirapuru' Kaszuba <uirapuruadg@gmail.com>
2018-02-22 20:03:30 +0100

    up

--- src/AppBundle/Form/InputType.php
+++ src/AppBundle/Form/InputType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,12 +15,12 @@ class InputType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("name", TextType::class)
-            ->add("position", TextType::class)
-            ->add("email", TextType::class)
-            ->add("mobile", TextType::class)
-            ->add("www", TextType::class)
-            ->add("submit", SubmitType::class)
+            ->add("name", TextType::class, ["label" => "Imię i nazwisko", "attr" => ["placeholder" => "np. Jan Kowalski"]])
+            ->add("position", TextType::class, ["label" => "Stanowisko", "attr" => ["placeholder" => "np. Programista PHP"]])
+            ->add("email", TextType::class, ["label" => "Adres email", "attr" => ["placeholder" => "np. jan@kowalski.pl"]])
+            ->add("mobile", TextType::class, ["label" => "Numer telefonu", "attr" => ["placeholder" => "np. +48 604 000 000"]])
+            ->add("www", HiddenType::class, ["data" => "http://coders.expert"])
+            ->add("submit", SubmitType::class, ["label" => "Wygeneruj stopkę!", "attr" => []])
         ;
     }
 
